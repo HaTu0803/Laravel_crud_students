80785f9a6c8c54fbeb7386d9ccf17a9ecacffb31
HaTu080302 <htttu0803@gmail.com>
2024-06-23 22:56:46 +0700

    v1

--- api/index.php
+++ api/index.php
@@ -1,10 +1,13 @@
-<?php 
-require __DIR__ . '/../vendor/autoload.php'; 
+<?php
+ 
+// Load the Laravel application
+require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-// Run the application 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class); 
-$response = $kernel->handle( 
+ 
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-$response->send(); 
+$response->send();
 $kernel->terminate($request, $response);
